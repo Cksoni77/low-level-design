33430fd40eafb5a2104962e0b062d15018e62d48
Cksoni77~ <cksoni3296@gmail.com>
2024-10-15 00:01:33 +0530

    added factory design pattern

---
+++ src/designpattern/factorydesignpattern/ShapeFactory.java
@@ -0,0 +1,15 @@
+package designpattern.factorydesignpattern;
+
+public class ShapeFactory {
+
+    public Shape getShape(String input) {
+       switch (input){
+           case "Circle":
+               return new Circle();
+           case "Rectangle":
+               return new Rectangle();
+           default:
+               return null;
+       }
+    }
+}
---
+++ src/designpattern/factorydesignpattern/Square.java
@@ -0,0 +1,8 @@
+package designpattern.factorydesignpattern;
+
+public class Square implements Shape{
+    @Override
+    public void draw() {
+        System.out.println("squre");
+    }
+}
